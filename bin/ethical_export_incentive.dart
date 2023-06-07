30f363a0a8f5e232df5a92140900699c519cc703
Riko <riktandil@gmail.com>
2023-06-07 14:43:10 +0700

    chore: add help param

--- bin/ethical_export_incentive.dart
+++ bin/ethical_export_incentive.dart
@@ -3,10 +3,24 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:ethical_export_incentive/ethical_export_incentive.dart' as ethical_export_incentive;
 
 const salesPeriodArg = 'period';
+const help = 'help';
+
+const salesPeriodHelp = r'''
+  Pass period argument in YYYY-MM-DD format.
+  Must not be null.
+''';
+
 void main(List<String> arguments) async {
-  final parser = ArgParser()..addOption(salesPeriodArg, abbr: 'p');
+  final parser = ArgParser()
+    ..addOption(salesPeriodArg, abbr: 'p', help: salesPeriodHelp)
+    ..addFlag(help, abbr: 'h', negatable: false);
 
   ArgResults argResults = parser.parse(arguments);
+
+  if (argResults[help]) {
+    print(parser.usage);
+    return;
+  }
 
   if (argResults[salesPeriodArg] == null) {
     throw Exception("Missing arguments");
